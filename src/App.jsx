10f181ca7bdc79bb94fc82ee021f0e9251459c8b
guru <guruprasad8424@gmail.com>
2022-01-25 13:35:36 +0530

    tic tac toe

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <h1>TIC TAC TOE</h1>
+    </div>
+  );
+};
+
+// export default () => (
+//   <>
+//     <h1>Welcome to React Vite Micro App!</h1>
+//     <p>Hard to get more minimal than this React app.</p>
+//   </>
+// );
+export default App;
